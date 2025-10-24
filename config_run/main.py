5c93707bf99cf189aa29bb4f39277ab5d92e6bf0
Filip Zemlicka <zemlifi1@fel.cvut.cz>
2025-10-24 15:46:07 +0200

    fix

--- config_run/main.py
+++ config_run/main.py
@@ -73,12 +73,12 @@ def get_cameras_from_config(cameras: dict):
 
 
 def main():
-    # if len(sys.argv) < 2:
-    #     log.error("Please provide the config file path, e.g. python train_sb3.py configs/isaacgym.yaml")
-    #     exit(1)
-    # config_name = sys.argv[1]
+    if len(sys.argv) < 2:
+        log.error("Please provide the config file path, e.g. python train_sb3.py configs/isaacgym.yaml")
+        exit(1)
+    config_name = sys.argv[1]
     #config_name = "g1_reach_pos_ori_train"
-    config_name = "g1_door_open_train"
+    #config_name = "g1_door_open_train"
     config = load_config_from_yaml(config_name)
     log.info(f"Loaded config: {config_name}")
 
